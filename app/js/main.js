61b8d8a705eaccdefbf94902003ff5e34988e003
krakehhh <67145116+krakehhh@users.noreply.github.com>
2021-06-24 09:14:06 +0300

    added old app

--- app/js/main.js
+++ app/js/main.js
@@ -145,3 +145,22 @@ $(function() {
   
 
 })
+
+const filterBox = document.querySelectorAll('.box')
+
+document.querySelector('.filter-color').addEventListener('click', event => {
+
+  if (event.target.tagName !== 'LABEL') return false;
+  let filterClass = event.target.dataset['color'];
+  // console.log(filterClass);
+
+  filterBox.forEach( elem => {
+    elem.classList.remove('.hide');
+    if (!elem.classList.contains(filterClass) && filterClass !== 'all') {
+      elem.classList.add('hide');
+    }
+  });
+
+});
+
+
